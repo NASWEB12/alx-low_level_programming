4ca93a769009bc21f3e0baf22ffc0c3f8771130d
NASWEB12 <aouade.98@gmail.com>
2023-07-14 04:28:27 -0700

    prints the alphabet in lowercase, and then in uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - prints the alphabet 
+ * main - Prints the alphabet.
  *
- * Return : Always 0 (Success
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
 	for (i = 0; i < 52; i++)
@@ -15,5 +16,6 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
+}
 }
 
